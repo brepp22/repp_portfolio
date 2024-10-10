98685d152b1025c844786ecfd22032d3a5d4bfe1
brepp22 <brepp@hotmail.com>
2024-10-09 19:41:02 -0700

    styling changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,13 @@ export default {
 			spacing : {
 				"5px" : "5px"
 		},
+			screens: {
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+		  },
 			animation: {
 				shake: 'shake 2s ease',
 			},
