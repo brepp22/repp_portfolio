ed938fe4504a0ce99276ef6e9f359bca34d61db9
brepp22 <brepp@hotmail.com>
2024-11-09 11:48:40 -0800

    netlify adapater added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import node from '@astrojs/node';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  base: '/',
+  adapter: netlify()
 });
