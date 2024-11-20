fd9131cc4832af30ee8e6faf4b9f6b06e71072c8
brepp22 <brepp@hotmail.com>
2024-11-20 12:01:33 -0800

    app page created with weather logo

---
+++ src/pages/api/sendEmail.json.ts
@@ -0,0 +1,75 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+
+console.log("RESEND_API_KEY:", import.meta.env.RESEND_API_KEY);
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const POST: APIRoute = async ({ request }) => {
+    try {
+        const body = await request.json();
+        const { name, userEmail, subject, message } = body;
+
+        console.log({ name, userEmail, subject, message  });
+
+        const response = await resend.emails.send({
+            to: "brepp@hotmail.com",
+            from: "noreply@brepp.dev" ,
+            subject,
+            html: `
+                From: ${userEmail}
+                Name: ${name}
+                Subject: ${subject}
+                Message: ${message}
+                
+            `,
+        });
+        
+        console.log('Resend response:', response);
+
+        if (response) {
+            return new Response(
+                JSON.stringify({
+                    message: 'Email sent successfully!',
+                }),
+                {
+                    status: 200,
+                    statusText: "OK",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Orgin": '*',
+                        "Access-Control-Allow-Methods": "POST",
+                    },
+                }
+            );
+        } else {
+            return new Response(
+                JSON.stringify({
+                    message: 'Failed to send email.',
+                }),
+                {
+                    status: 500,
+                    statusText: "Internal Server Error",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+        }
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return new Response(
+            JSON.stringify({
+                message: 'An error occurred while sending the email.',
+            }),
+            {
+                status: 500,
+                statusText: "Internal Server Error",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+    }
+};
+
